daec43c8fa34628b90343562bd45e155ba3fe4f2
Nhan <nhtrnguy@ucsc.edu>
2023-10-15 13:22:22 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -7,5 +7,13 @@ const gameName = "Nhan's game";
 document.title = gameName;
 
 const header = document.createElement("h1");
+const canvas = document.createElement("canvas");
+
+canvas.id = "canvas";
+const ctx = canvas.getContext("2d");
+ctx!.fillStyle = "white";
+ctx!.fillRect(0, 0, 1000, 1000);
+
 header.innerHTML = gameName;
 app.append(header);
+app.append(canvas);
