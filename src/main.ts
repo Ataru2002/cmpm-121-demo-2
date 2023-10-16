e6cbcb241e6a3fbd610d8d5697e952603d7f54c9
Nhan <nhtrnguy@ucsc.edu>
2023-10-15 18:14:54 -0700

    step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,18 @@ document.title = gameName;
 const header = document.createElement("h1");
 const canvas = document.createElement("canvas");
 const clrbt = document.createElement("button");
+const undobt = document.createElement("button");
+const redobt = document.createElement("button");
+
 const bus = new EventTarget();
-let currentPath: Array<[number, number]> | null = null;
-const paths: Array<Array<[number, number]>> = [];
+let currentPath: [number, number][] | null = null;
+const paths: [number, number][][] = [];
+const redos: [number, number][][] = [];
+let insert: [number, number][] | undefined;
 
 clrbt.innerHTML = "clear";
+undobt.innerHTML = "undo";
+redobt.innerHTML = "redo";
 canvas.id = "canvas";
 canvas.height = 256;
 canvas.width = 256;
@@ -27,6 +34,7 @@ canvas.addEventListener("mousedown", (current) => {
   cursor.active = true;
   currentPath = [];
   paths.push(currentPath);
+  redos.splice(0, redos.length);
   currentPath?.push([current.offsetX, current.offsetY]);
   notify(new Event("drawing-changed"));
 });
@@ -45,14 +53,38 @@ canvas.addEventListener("mouseup", () => {
   currentPath = null;
   notify(new Event("drawing-changed"));
 });
+
 clrbt.addEventListener("click", () => {
   ctx?.clearRect(0, 0, canvas.width, canvas.height);
   paths.splice(0, paths.length);
 });
+
+undobt.addEventListener("click", () => {
+  if (paths.length > 0) {
+    insert = paths.pop();
+    if (insert) {
+      redos.push(insert);
+    }
+    notify(new Event("drawing-changed"));
+  }
+});
+
+redobt.addEventListener("click", () => {
+  if (redos.length > 0) {
+    insert = redos.pop();
+    if (insert) {
+      paths.push(insert);
+    }
+    notify(new Event("drawing-changed"));
+  }
+});
+
 header.innerHTML = gameName;
 app.append(header);
 app.append(canvas);
 app.append(clrbt);
+app.append(undobt);
+app.append(redobt);
 
 function notify(name: Event) {
   bus.dispatchEvent(name);
